42224ee5c53ffec032c4c9d4d7f2a6d10803bcf5
Thdeathz <buidunga112@gmail.com>
2022-11-04 02:10:04 +0700

    refactor lobby.html to index.html

--- js/room_rtc.js
+++ js/room_rtc.js
@@ -22,7 +22,7 @@ if (!roomId) {
 
 let displayName = sessionStorage.getItem('display_name')
 if (!displayName) {
-  window.location = 'lobby.html'
+  window.location = 'index.html'
 }
 
 let localTracks = []
